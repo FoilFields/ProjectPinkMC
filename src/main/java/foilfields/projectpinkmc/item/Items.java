5ebeb300a69cfb609653eff86c4976464a69ac65
Woukie <lucaseldon@live.com>
2022-04-12 17:40:12 +0100

    Added pig soul

--- src/main/java/foilfields/projectpinkmc/item/Items.java
+++ src/main/java/foilfields/projectpinkmc/item/Items.java
@@ -11,6 +11,7 @@ public class Items {
     public static final Item VIAL = register("vial", new Item(new Item.Settings().group(ProjectPinkMC.CREATIVE_GROUP)));
     public static final Item PIG_POLYMORPH = register("pig_polymorph", new PigPolymorph(new Item.Settings().recipeRemainder(VIAL).food(FoodComponents.APPLE).group(ProjectPinkMC.CREATIVE_GROUP).maxCount(16)));
     public static final Item SOUL_SHEARS = register("soul_shears", new SoulShears(new Item.Settings().maxDamage(25).group(ProjectPinkMC.CREATIVE_GROUP)));
+    public static final Item PIG_SOUL = register("pig_soul", new Item(new Item.Settings().group(ProjectPinkMC.CREATIVE_GROUP)));
 
     private static Item register(String id, Item item) {
         return register(new Identifier(ProjectPinkSettings.MOD_ID, id), item);

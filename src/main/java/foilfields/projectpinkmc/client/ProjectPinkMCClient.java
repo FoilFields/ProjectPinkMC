e400f77c19e52acd4d3a55c5495fafdb5953c11d
Woukie <lucaseldon@live.com>
2022-04-13 01:06:57 +0100

    Removed unused imports

--- src/main/java/foilfields/projectpinkmc/client/ProjectPinkMCClient.java
+++ src/main/java/foilfields/projectpinkmc/client/ProjectPinkMCClient.java
@@ -3,8 +3,6 @@ package foilfields.projectpinkmc.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Environment(EnvType.CLIENT)
 public class ProjectPinkMCClient implements ClientModInitializer {

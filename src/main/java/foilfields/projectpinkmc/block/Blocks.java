5002c82066791f22f44bc4878e06d0a9540f0970
Jamie Markiewicz <75029974+Devilfish323@users.noreply.github.com>
2022-04-14 23:28:42 +0100

    made dicer mineable

--- src/main/java/foilfields/projectpinkmc/block/Blocks.java
+++ src/main/java/foilfields/projectpinkmc/block/Blocks.java
@@ -11,7 +11,7 @@ import net.minecraft.util.registry.Registry;
 
 public class Blocks {
     public static final Block NIGHTSHADE_BUSH = Blocks.register("nightshade_bush", new NightshadeBushBlock(AbstractBlock.Settings.of(Material.PLANT).ticksRandomly().noCollision().sounds(BlockSoundGroup.SWEET_BERRY_BUSH)));
-    public static final Block DICER = register("dicer", new Block(FabricBlockSettings.of(Material.STONE).strength(4.0f)));
+    public static final Block DICER = register("dicer", new Block(FabricBlockSettings.of(Material.STONE).strength(4.0f).requiresTool()));
 
     private static Block register(String id, Block block) {
         return Registry.register(Registry.BLOCK, new Identifier(ProjectPinkSettings.MOD_ID, id), block);

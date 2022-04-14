162590d616a8271af206b3ae2f7c369a2e384d6b
Woukie <lucaseldon@live.com>
2022-04-14 16:05:17 +0100

    Pig effect hitbox and eye height
    Change player hitbox and eye height when under the pig effect

--- src/main/java/foilfields/projectpinkmc/statuseffect/PigEffect.java
+++ src/main/java/foilfields/projectpinkmc/statuseffect/PigEffect.java
@@ -1,9 +1,16 @@
 package foilfields.projectpinkmc.statuseffect;
 
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.AttributeContainer;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
+import virtuoel.pehkui.api.ScaleData;
+import virtuoel.pehkui.api.ScaleTypes;
 
 public class PigEffect extends StatusEffect {
+    ScaleData scaleDataWidth, scaleDataHeight, scaleDataEyeHeight;
+
     protected PigEffect() {
         super(StatusEffectCategory.NEUTRAL, 0x000000);
     }
@@ -11,5 +18,25 @@ public class PigEffect extends StatusEffect {
     @Override
     public boolean canApplyUpdateEffect(int duration, int amplifier) {
         return true;
+    }
+
+    @Override
+    public void onRemoved(LivingEntity entity, AttributeContainer attributes, int amplifier) {
+        scaleDataWidth.setScale(1);
+        scaleDataHeight.setScale(1);
+        scaleDataEyeHeight.setScale(1);
+    }
+
+    @Override
+    public void onApplied(LivingEntity entity, AttributeContainer attributes, int amplifier) {
+        scaleDataWidth = ScaleData.Builder.create().entity(entity).type(ScaleTypes.HITBOX_WIDTH).build();
+        scaleDataHeight = ScaleData.Builder.create().entity(entity).type(ScaleTypes.HITBOX_HEIGHT).build();
+        scaleDataEyeHeight = ScaleData.Builder.create().entity(entity).type(ScaleTypes.EYE_HEIGHT).build();
+
+        float heightScale = EntityType.PIG.getHeight() / EntityType.PLAYER.getHeight();
+
+        scaleDataWidth.setScale(EntityType.PIG.getWidth() / EntityType.PLAYER.getWidth());
+        scaleDataHeight.setScale(heightScale);
+        scaleDataEyeHeight.setScale(heightScale);
     }
 }
